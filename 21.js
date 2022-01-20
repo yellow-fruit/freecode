75d331ebb095934ce49d4669ab54585004ec4c90
Nikolai_Golikov <96423480+yellow-fruit@users.noreply.github.com>
2022-01-21 00:42:46 +0300

    delete two

--- 21.js
+++ 21.js
@@ -1,4 +1,4 @@
 const lastName = "Lovelace";
 
 // Only change code below this line
-const secondToLastLetterOfLastName2 = lastName[lastName.length - 2]; // Change this line
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
